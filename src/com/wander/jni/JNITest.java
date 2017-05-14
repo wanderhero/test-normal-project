ec94a6d6d32fc81a5b8c6e8c7a337ff083e1156c
wanderhero <cengjingsea@163.com>
2017-05-14 20:31:56 +0800

    整理 jni 文件

--- src/com/wander/jni/JNITest.java
+++ src/com/wander/jni/JNITest.java
@@ -2,12 +2,12 @@ package com.wander.jni;
 
 public class JNITest {
 	static {
-		System.loadLibrary("JNITest");// 加载实现了native函数的动态库
+		System.loadLibrary("JNITest");// 加载实现了 native 函数的动态库
 	}
 
-	public static native String test(String name);// 声明native函数
+	public static native String test(String name);// 声明 native 函数
 
 	public static void main(String[] args) {
-		System.out.println(test("wander"));// 调用本地函数
+		System.out.println(test("wander"));// 调用 native 函数
 	}
 }
