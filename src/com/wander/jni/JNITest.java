fe56ae096367b0455d133b01b390d117be4fede1
wanderhero <cengjingsea@163.com>
2017-05-19 17:59:37 +0800

    代码整理

--- src/com/wander/jni/JNITest.java
+++ src/com/wander/jni/JNITest.java
@@ -1,6 +1,7 @@
 package com.wander.jni;
 
 public class JNITest {
+
 	static {
 		System.loadLibrary("JNITest");// 加载实现了 native 函数的动态库
 	}
@@ -10,4 +11,5 @@ public class JNITest {
 	public static void main(String[] args) {
 		System.out.println(test("wander"));// 调用 native 函数
 	}
+
 }
--- src/com/wander/jni/JavaLibraryPath.java
+++ src/com/wander/jni/JavaLibraryPath.java
@@ -1,7 +1,9 @@
 package com.wander.jni;
 
 public class JavaLibraryPath {
+
 	public static void main(String[] args) {
 		System.out.println(System.getProperty("java.library.path"));
 	}
+
 }
