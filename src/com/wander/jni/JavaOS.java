8e182077a30cdfee1c82b5b60dd1c33764175dc7
wanderhero <cengjingsea@163.com>
2017-05-19 17:59:24 +0800

    判断是什么系统

---
+++ src/com/wander/jni/JavaOS.java
@@ -0,0 +1,11 @@
+package com.wander.jni;
+
+public class JavaOS {
+
+	public static void main(String[] args) {
+		System.out.println(System.getProperty("os.name"));
+		System.out.println(System.getProperty("os.version"));
+		System.out.println(System.getProperty("os.arch"));
+	}
+
+}
